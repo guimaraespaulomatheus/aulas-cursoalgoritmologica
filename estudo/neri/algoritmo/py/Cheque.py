6b22b20dfd6a877730803ebde4d307f0b137a401
Paulo Matheus Guimarães Jouguet <paulomatheus@iMac-de-PALLOMA.local>
2023-11-11 15:03:42 -0300

    Alterando a segunda linha

--- estudo/neri/algoritmo/py/Cheque.py
+++ estudo/neri/algoritmo/py/Cheque.py
@@ -1,4 +1,4 @@
 saldo = 500
-cheque = float(input("Informe o valor de entrada"))
+cheque = float(input("Informe o valor de entrada: "))
 saldo_final = saldo - cheque
 print(f"Informe o saldo final: {saldo_final}")
